acdaa39fdcb082d412b98c417e350835ec730ccf
sschlingmann <sschlingmann@ufl.edu>
2023-10-25 20:48:38 -0400

    added decode function and new option in main function -scott

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+# Joshua Berelson
 def encode(password):       #Takes in integer password in a string format and encodes it, shifting each digit by 3
     result = ''
     for i in password:
         result += str((int(i)+3) % 10)
+    return result
+
+
+def decode(password):  # Takes in integer password as a string and decodes it back to original string. -Scott
+    password = encode(password)
+    result = ''
+    for i in password:
+        result += str((int(i)+7) % 10)
     return result
 
 
@@ -14,8 +23,8 @@ def main():          #Main function
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
 
-        if user_input == 2:
-            pass
+        if user_input == 2:  # Prints out encoded string and decodes it back to the original string. -Scott
+            print(f'The encoded password is {encode(password)}, and the original password is {decode(password)}.')
 
         if user_input == 3:
             exit()
